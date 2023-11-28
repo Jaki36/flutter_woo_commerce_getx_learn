0490630e81d776039f70a5e1726f61bbb90a32ed
LiDa <77716823+kendy963@users.noreply.github.com>
2023-11-28 16:43:45 +0900

    native 启动图 flutter_native_splash

--- lib/global.dart
+++ lib/global.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 import 'common/index.dart';
@@ -6,7 +7,8 @@ import 'common/services/index.dart';
 
 class Global {
   static Future<void> init() async {
-    WidgetsFlutterBinding.ensureInitialized();  //  这个表示先就行原生端（ios android）插件注册，然后再处理后续操作，这样能保证代码运行正确。
+    WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();  //  这个表示先就行原生端（ios android）插件注册，然后再处理后续操作，这样能保证代码运行正确。
+    FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);  // 启动 native splash
 
     await Future.wait([
       Get.putAsync<ConfigService>(() async => await ConfigService().init()),
